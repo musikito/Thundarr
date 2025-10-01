8cc742fbdbdacc2c48967a648745ad6f7b408012
JoséMiguel Marte <tumusicalatina@gmail.com>
2025-09-30 21:38:53 -0400

    Add camera

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,17 +2,28 @@ extends CharacterBody2D
 
 
 @export var speed : float = 200.0
-@export var jump_velocity : float = -200.0
+@export var jump_velocity : float = -150.0
+@export var double_jump_velocity : float = -100.0
+
+var has_double_jump : bool = false
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else:
+		has_double_jump = false
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_velocity
+	if Input.is_action_just_pressed("jump"):
+		if is_on_floor():
+			# Normal jump
+			velocity.y = jump_velocity
+		elif not has_double_jump:
+			# Perform double jump
+			velocity.y = double_jump_velocity
+			has_double_jump = true
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
